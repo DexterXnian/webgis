9f35adb6e24a59a474260fa4fdbb2927ad57ad79
Dexter <dexternian@qq.com>
2023-10-07 11:52:17 +0800

    config --global https.proxy http://127.0.0.1:7890

--- scripts/main.js
+++ scripts/main.js
@@ -1,46 +1,52 @@
 
-let myButton = document.querySelector("button");
+//let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
-let myImage = document.querySelector("img");
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+
 
 
 if (!localStorage.getItem("name")) {
-    setUserName();
+  setUserName();
 } else {
-    let storedName = localStorage.getItem("name");
-    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla 酷毙了，" + storedName;
 }
 
+let myImage = document.querySelector("img");
+let imageArray = ["images/index.png", "images/index2.png"];
+let currentIndex = 0;
+
 myImage.onclick = function () {
-    let mySrc = myImage.getAttribute("src");
-    if (mySrc === "./images/index.png") {
-        myImage.setAttribute("src", "images/index2.png");
-    } else {
-        myImage.setAttribute("src", "images/index.png");
-    }
+  currentIndex = (currentIndex + 1) % imageArray.length;
+  myImage.setAttribute("src", imageArray[currentIndex]);
 };
+
 
 
 function setUserName() {
-    let myName = prompt("请输入你的名字。");
-    if (!myName) {
-      setUserName();
-    } else {
-      localStorage.setItem("name", myName);
-      myHeading.textContent = "Mozilla 酷毙了，" + myName;
-    }
-  }
-  
-
-window.onbeforeunload = function() {
-    // 在页面关闭前清除 localStorage 中的名字数据
-    localStorage.removeItem("name");
-    e.returnValue = "你确定要离开吗？";
-  };
-  
-
-myButton.onclick = function () {
+  let myName = prompt("请输入你的名字。");
+  if (!myName) {
     setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+  }
+}
+
+
+// window.onbeforeunload = function () {
+//   // 在页面关闭前清除 localStorage 中的名字数据
+//   localStorage.removeItem("name");
+//   e.returnValue = "你确定要离开吗？";
+// };
+
+
+button1.onclick = function () {
+  setUserName();
 };
 
 
+button2.onclick = function () {
+  window.location.href = "map.html";
+};
